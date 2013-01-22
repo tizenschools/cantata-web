5e83c1586a4d4c5923eda380f7bd5a95bb92c589
gyeongseok.seo <gyeongseok.seo@samsung.com>
2013-01-22 20:36:43 +0900

    [Title] Added using mock data logic.

--- public/js/System.js
+++ public/js/System.js
@@ -18,6 +18,7 @@
 
         initialize: function() {
             Logger.trace( 'system info view init' );
+            this.model.bind( 'change', this.createContents, this );
             this.model.fetch();
             this.$storageChart = $( this.template( this.model ) );
         },
@@ -29,9 +30,6 @@
         createContents: function() {
             Logger.trace( 'create system info view' );
             var storage = this.model;
-            Logger.log( storage );
-            Logger.log( storage.get( 'total' ) + ',' + storage.attributes.total );
-            Logger.log( storage.toJSON() );
             var t = this.model.get( 'total' );
             var u = this.model.get( 'usage' );
             var r = this.model.get( 'remainder' );
@@ -39,32 +37,32 @@
             Logger.trace( 'Total: ' + t + ', Usage: ' + u + ', Remain: ' + r );
 
             this.contents.append( this.$storageChart );
-            var s1 = [['Sony',7], ['Samsumg',13.3], ['LG',14.7], ['Vizio',5.2], ['Insignia', 1.2]];
+            var data = [['Usage',Number(u)], ['Remain',Number(r)] ];
          
-    var plot8 = $.jqplot('storage-pie', [s1], {
-        grid: {
-            drawBorder: false, 
-            drawGridlines: false,
-            background: '#ffffff',
-            shadow:false
-        },
-        axesDefaults: {
-             
-        },
-        seriesDefaults:{
-            renderer:$.jqplot.PieRenderer,
-            rendererOptions: {
-                showDataLabels: true
-            }
-        },
-        legend: {
-            show: true,
-            rendererOptions: {
-                numberRows: 1
-            },
-            location: 's'
-        }
-    }); 
+            var chart = $.jqplot('storage-pie', [data], {
+                    grid: {
+                    drawBorder: false, 
+                    drawGridlines: false,
+                    background: '#ffffff',
+                    shadow:false
+                },
+                axesDefaults: {
+                 
+                },
+                seriesDefaults:{
+                    renderer:$.jqplot.PieRenderer,
+                    rendererOptions: {
+                        showDataLabels: true
+                    }
+                },
+                legend: {
+                    show: true,
+                    rendererOptions: {
+                        numberRows: 1
+                    },
+                    location: 's'
+                }
+            }); 
         }
     } );
 
